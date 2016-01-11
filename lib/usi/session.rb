f3e249089b86d3ae6580a9eefc43268bafcff0fb
mgi166 <skskoari@gmail.com>
2016-01-11 23:48:11 +0900

    Use Open3.popen3 in execution of engine.

--- lib/usi/session.rb
+++ lib/usi/session.rb
@@ -1,9 +1,12 @@
+require "open3"
+
 module USI
   class Session
-    attr_reader :engine
+    attr_reader :engine, :stdin, :stdout, :stderr, :wait_thr
 
     def initialize(engine)
       @engine = engine
+      @stdin, @stdout, @stderr, @wait_thr = *Open3.popen3(@engine.engine_path.to_s, chdir: @engine.engine_path.dirname)
     end
   end
 end
