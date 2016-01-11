b494eaa4d1af80f4f60afc056ad69e312aed9efb
mgi166 <skskoari@gmail.com>
2016-01-11 15:44:04 +0900

    Validate to existance of engine.

--- lib/usi/engine.rb
+++ lib/usi/engine.rb
@@ -1,9 +1,17 @@
 module USI
   class Engine
+    class NoSuchEngineError < ::LoadError; end
+
     attr_reader :engine_path
 
     def initialize(engine_path)
-      @engine_path = Pathname.new(engine_path)
+      @engine_path = Pathname.new(engine_path).expand_path
+    end
+
+    def validate!
+      unless @engine_path.exist?
+        raise NoSuchEngineError, "No such engine to shougi -- #{@engine_path}"
+      end
     end
   end
 end
--- spec/usi/engine_spec.rb
+++ spec/usi/engine_spec.rb
@@ -6,4 +6,14 @@ describe USI::Engine do
       expect(engine.engine_path).to eq Pathname.new("path/to/engine")
     end
   end
+
+  describe "#validate!" do
+    context 'the engine does not exist' do
+      it do
+        expect do
+          USI::Engine.new("path/to/engine").validate!
+        end.to raise_error USI::Engine::NoSuchEngineError
+      end
+    end
+  end
 end
