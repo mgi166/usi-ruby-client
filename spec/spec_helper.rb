325cdb22492a645f89e1a21378f22b79c40388f0
mgi166 <skskoari@gmail.com>
2016-01-11 15:18:23 +0900

    require some source codes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,10 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require "pathname"
+require Pathname.new(__dir__).join("../lib/usi-client.rb")
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
