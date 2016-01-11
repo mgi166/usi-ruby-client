4cda96853bb6ddee24de13fcdc82325ff3cede59
mgi166 <skskoari@gmail.com>
2016-01-11 15:22:30 +0900

    Add USI::Engine#engine_path
    * Getter of engine path

--- lib/usi/engine.rb
+++ lib/usi/engine.rb
@@ -1,4 +1,9 @@
 module USI
   class Engine
+    attr_reader :engine_path
+
+    def initialize(engine_path)
+      @engine_path = Pathname.new(engine_path)
+    end
   end
 end
--- spec/usi/engine_spec.rb
+++ spec/usi/engine_spec.rb
@@ -1,2 +1,9 @@
 describe USI::Engine do
+  describe "#engine_path" do
+    let(:engine) { USI::Engine.new("path/to/engine") }
+
+    it "returns pathname of engine" do
+      expect(engine.engine_path).to eq Pathname.new("path/to/engine")
+    end
+  end
 end
