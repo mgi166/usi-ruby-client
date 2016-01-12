4fdf522bbab8e1d0025107fcb884df0963bb78c1
mgi166 <skskoari@gmail.com>
2016-01-12 23:11:01 +0900

    Parse output and set attributes.
    * Add #usiok?

--- lib/usi/response.rb
+++ lib/usi/response.rb
@@ -1,4 +1,23 @@
 module USI
   class Response
+    attr_reader :output
+
+    def initialize(output)
+      @output = output
+      parse
+    end
+
+    def parse
+      output.split("\n").each do |line|
+        case line
+        when /^usiok$/
+          @usiok = true
+        end
+      end
+    end
+
+    def usiok?
+      !! @usiok
+    end
   end
 end
--- spec/usi/response_spec.rb
+++ spec/usi/response_spec.rb
@@ -1,2 +1,29 @@
 describe USI::Response do
+  describe '#usiok?' do
+    let(:response) { USI::Response.new(output) }
+
+    context 'when output includes `usiok`' do
+      let(:output) do
+        <<-EOS
+id name Apery
+id author Hiraoka Takuya
+usiok
+        EOS
+      end
+
+      it do
+        expect(response.usiok?).to be true
+      end
+    end
+
+    context 'when output does not include `usiok`' do
+      let(:output) do
+        ""
+      end
+
+      it do
+        expect(response.usiok?).to be false
+      end
+    end
+  end
 end
