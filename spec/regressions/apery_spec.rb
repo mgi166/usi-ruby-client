9b15c8b725a204e46da3e95bad144d4f72f97cbf
mgi166 <skskoari@gmail.com>
2016-01-17 18:01:47 +0900

    Prepare to regression spec

--- spec/regressions/apery_spec.rb
+++ spec/regressions/apery_spec.rb
@@ -1,2 +1,9 @@
 describe "Apery" do
+  let(:repository_root) { Pathname.new(__dir__).join("../..") }
+  let(:apery_path) { repository_root.join("../apery/bin/apery") }
+
+  it do
+    # do something
+    client = USI::Client.new(apery_path)
+  end
 end
