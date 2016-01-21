7408f431161e5b8f32f5c61506eba3fc522016c3
mgi166 <skskoari@gmail.com>
2016-01-21 22:25:00 +0900

    Add USI::Resource::Id.create
    * Create instance. but parse the arguments

--- lib/usi/resource/id.rb
+++ lib/usi/resource/id.rb
@@ -1,4 +1,33 @@
 module USI::Resource
   class Id
+    def self.parse(args)
+      command, arg = args.split(" ", 2)
+
+      if command != "name" || command != "author"
+        # TODO: log or raise
+      end
+
+      # NOTE: `command` is `name` or `author`.
+      { command => arg }
+    end
+
+    def self.create(args)
+      attr = parse(args)
+      new(attr)
+    end
+
+    def initialize(attributes)
+      assign_attributes(attributes)
+    end
+
+    attr_accessor :name, :author
+
+    private
+
+    def assign_attributes(attributes)
+      attributes.each do |attr_name, value|
+        self.send("#{attr_name}=", value)
+      end
+    end
   end
 end
--- spec/usi/resource/id_spec.rb
+++ spec/usi/resource/id_spec.rb
@@ -1,2 +1,49 @@
 describe USI::Resource::Id do
+  describe '.create' do
+    context 'return value' do
+      let(:args) { "name hoge" }
+
+      it do
+        expect(USI::Resource::Id.create(args)).to be_instance_of USI::Resource::Id
+      end
+    end
+
+    context '`name` is included in arguments' do
+      let(:args) { "name hoge" }
+
+      it "`name` is the value specified an argument" do
+        expect(
+          USI::Resource::Id.create(args).name
+        ).to eq "hoge"
+      end
+    end
+
+    context '`author` is included in arguments' do
+      let(:args) { "author fuga" }
+
+      it "`author` is the value specified an argument" do
+        expect(
+          USI::Resource::Id.create(args).author
+        ).to eq "fuga"
+      end
+    end
+
+    context '`name` or `author` does not includ in arguments' do
+      let(:args) { "aaa bbbb" }
+
+      it do
+        skip "TODO: Not determing"
+      end
+    end
+
+    context 'many spaces is included' do
+      let(:args) { "name       aaa bb  ccc" }
+
+      it "ignores after `name` spaces" do
+        expect(
+          USI::Resource::Id.create(args).name
+        ).to eq "aaa bb  ccc"
+      end
+    end
+  end
 end
