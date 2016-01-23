4d8e3e5584965e93ec4888832f852f680e766c1a
mgi166 <skskoari@gmail.com>
2016-01-23 19:32:01 +0900

    Add USI::Resource::Bestmove.parse, parse options for bestmove.

--- lib/usi/resource/bestmove.rb
+++ lib/usi/resource/bestmove.rb
@@ -1,4 +1,19 @@
 module USI::Resource
   class Bestmove
+    def self.parse(args)
+      ponder = nil
+      bestmove, ponders = args.split(" ", 2)
+
+      if ponders
+        command, move = ponders.split(" ", 2)
+        if command == "ponder"
+          ponder = { "ponder" => move }
+        else
+          # TODO: raise or log
+        end
+      end
+
+      [bestmove, ponder]
+    end
   end
 end
--- spec/usi/resource/bestmove_spec.rb
+++ spec/usi/resource/bestmove_spec.rb
@@ -1,2 +1,33 @@
 describe USI::Resource::Bestmove do
+  describe '.parse' do
+    context 'does not include `ponder`' do
+      subject { USI::Resource::Bestmove.parse(args) }
+
+      let(:args) { "8c8d" }
+
+      it do
+        is_expected.to eq ["8c8d", nil]
+      end
+    end
+
+    context 'includes `ponder`' do
+      subject { USI::Resource::Bestmove.parse(args) }
+
+      let(:args) { "8c8d ponder None" }
+
+      it do
+        is_expected.to eq ["8c8d", "ponder" => "None"]
+      end
+    end
+
+    context 'includes `ponder` multiple values' do
+      subject { USI::Resource::Bestmove.parse(args) }
+
+      let(:args) { "8c8d ponder 3a2b 8c7d" }
+
+      it do
+        is_expected.to eq ["8c8d", "ponder" => "3a2b 8c7d"]
+      end
+    end
+  end
 end
