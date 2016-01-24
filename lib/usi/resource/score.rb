6c36c3459f5b747dfd531901b4d4f15fc11e4d84
mgi166 <skskoari@gmail.com>
2016-01-24 19:18:17 +0900

    Set cp, getter returns Fixnum cp.

--- lib/usi/resource/score.rb
+++ lib/usi/resource/score.rb
@@ -1,4 +1,27 @@
 module USI::Resource
   class Score
+    def initialize(args)
+      assign_attributes(args)
+    end
+
+    attr_accessor :cp, :mate, :state
+
+    def cp=(value)
+      @cp = value.to_f
+    end
+
+    private
+
+    def assign_attributes(args)
+      case args.size
+      when 2
+        send("#{args[0]}=", args[1])
+      when 3
+        send("#{args[0]}=", args[1])
+        @state = args[2]
+      else
+        # log and raise
+      end
+    end
   end
 end
--- spec/usi/resource/score_spec.rb
+++ spec/usi/resource/score_spec.rb
@@ -1,2 +1,29 @@
 describe USI::Resource::Score do
+  describe '#cp' do
+    let(:score) { USI::Resource::Score.new(args) }
+
+    context '`cp` is included' do
+      let(:args) { ["cp", "-1521"] }
+
+      it "returns the cp(centi pawn) value on Fixnum" do
+        expect(score.cp).to eq -1521
+      end
+    end
+
+    context '`cp` is not included' do
+      let(:args) { ["mate", "+"] }
+
+      it do
+        expect(score.cp).to be nil
+      end
+    end
+
+    context '`lowerbound` or `upperbound` is included' do
+      let(:args) { ["cp", "-9999", "lowerbound"] }
+
+      it "returns the cp(centi pawn) value on Fixnum" do
+        expect(score.cp).to eq -9999
+      end
+    end
+  end
 end
