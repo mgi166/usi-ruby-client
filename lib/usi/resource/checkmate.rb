d0c8ec287b590bd8be7e62e708bc87fcd0bf6540
mgi166 <skskoari@gmail.com>
2016-01-24 12:43:20 +0900

    Add USI::Resource::Checkmate.parse
    * parse arguments

--- lib/usi/resource/checkmate.rb
+++ lib/usi/resource/checkmate.rb
@@ -1,4 +1,18 @@
 module USI::Resource
   class Checkmate
+    def self.parse(args)
+      state = nil
+      moves = args.split(" ")
+
+      if checkmate_puzzle?(moves.first)
+        state = moves.shift
+      end
+
+      [moves, state]
+    end
+
+    def self.checkmate_puzzle?(value)
+      %w(notimplemented timeout nomate).include?(value)
+    end
   end
 end
--- spec/usi/resource/checkmate_spec.rb
+++ spec/usi/resource/checkmate_spec.rb
@@ -1,2 +1,23 @@
 describe USI::Resource::Checkmate do
+  describe '.parse' do
+    context 'implemented checkmate puzzle' do
+      subject { USI::Resource::Checkmate.parse(args) }
+
+      let(:args) { "G*8f 9f9g 8f8g 9g9h 8g8h" }
+
+      it do
+        is_expected.to eq [%w(G*8f 9f9g 8f8g 9g9h 8g8h), nil]
+      end
+    end
+
+    context 'not implemented checkmate puzzle' do
+      subject { USI::Resource::Checkmate.parse(args) }
+
+      let(:args) { "notimplemented" }
+
+      it do
+        is_expected.to eq [[], "notimplemented"]
+      end
+    end
+  end
 end
