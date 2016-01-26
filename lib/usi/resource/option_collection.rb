77056b510a877256208ae92c7d0586344d93b9aa
mgi166 <skskoari@gmail.com>
2016-01-26 21:35:14 +0900

    Add USI::Resource::OptionCollection.create
    * create Option instance, and cache in self.

--- lib/usi/resource/option_collection.rb
+++ lib/usi/resource/option_collection.rb
@@ -1,4 +1,19 @@
 module USI::Resource
   class OptionCollection
+    def self.create(args)
+      option = Option.create(args)
+      new(option)
+    end
+
+    def initialize(option)
+      @options = {}
+      add(option)
+    end
+
+    attr_reader :options
+
+    def add(option)
+      options[option.name] = option
+    end
   end
 end
--- spec/usi/resource/option_collection_spec.rb
+++ spec/usi/resource/option_collection_spec.rb
@@ -1,2 +1,15 @@
 describe USI::Resource::OptionCollection do
+  describe '.create' do
+    subject { USI::Resource::OptionCollection.create(args) }
+
+    let(:args) { "name ResetLearning type button" }
+
+    it do
+      is_expected.to be_instance_of USI::Resource::OptionCollection
+    end
+
+    it "creates USI::Resource::Option instance, and store in self" do
+      expect(subject.options['ResetLearning']).to be_instance_of USI::Resource::Option
+    end
+  end
 end
