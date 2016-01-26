c51db148a0e9e0b7409d00d3429615f381ae3692
mgi166 <skskoari@gmail.com>
2016-01-26 21:34:31 +0900

    require "usi/resource/option_collection"

--- lib/usi/resource.rb
+++ lib/usi/resource.rb
@@ -1,5 +1,6 @@
 require "usi/resource/id"
 require "usi/resource/option"
+require "usi/resource/option_collection"
 require "usi/resource/bestmove"
 require "usi/resource/checkmate"
 require "usi/resource/info"
