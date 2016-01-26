9740c07c4e407f3a507c851a82c86aba22123c51
mgi166 <skskoari@gmail.com>
2016-01-26 21:48:41 +0900

    Add USI::Resource::OptionCollection#update

--- lib/usi/resource/option_collection.rb
+++ lib/usi/resource/option_collection.rb
@@ -12,8 +12,18 @@ module USI::Resource
 
     attr_reader :options
 
+    def update(args)
+      option = Option.create(args)
+      add(option)
+      self
+    end
+
     def add(option)
       options[option.name] = option
+    end
+
+    def [](option_name)
+      options[option_name]
     end
   end
 end
--- spec/usi/resource/option_collection_spec.rb
+++ spec/usi/resource/option_collection_spec.rb
@@ -12,4 +12,28 @@ describe USI::Resource::OptionCollection do
       expect(subject.options['ResetLearning']).to be_instance_of USI::Resource::Option
     end
   end
+
+  describe '#update' do
+    let(:collection) { USI::Resource::OptionCollection.new(option) }
+    let(:option) { USI::Resource::Option.create("name ResetLearning type button") }
+
+    context 'receive new option' do
+      let(:args) { "name Best_Book_Move type check default false" }
+
+      it "create new option, and cache" do
+        collection.update(args)
+        expect(collection.options.keys).to eq %w(ResetLearning Best_Book_Move)
+      end
+    end
+
+    context 'receive exists option' do
+      let(:args) { "name ResetLearning type spin" }
+
+      it "override this option" do
+        collection.update(args)
+        expect(collection.options.keys).to eq %w(ResetLearning)
+        expect(collection['ResetLearning'].type).to eq "spin"
+      end
+    end
+  end
 end
