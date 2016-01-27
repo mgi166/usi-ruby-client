b1b077c7f2031f7067e63d276d7145cd67e94d8c
mgi166 <skskoari@gmail.com>
2016-01-27 22:59:56 +0900

    Add same specs for USI::Client
    * It is better than none specs.

--- spec/usi/client_spec.rb
+++ spec/usi/client_spec.rb
@@ -7,6 +7,61 @@ describe USI::Client do
     end
   end
 
+  describe '#usi' do
+    let(:client) { USI::Client.new("spec/bin/dummy_engine") }
+
+    it "call USI::Engine#command with `usi`" do
+      expect(client).to receive(:command).with("usi")
+      client.usi
+    end
+  end
+
+  describe '#isready' do
+    let(:client) { USI::Client.new("spec/bin/dummy_engine") }
+
+    it "call USI::Engine#command with `isready`" do
+      expect(client).to receive(:command).with("isready")
+      client.isready
+    end
+  end
+
+  describe '#usinewgame' do
+    let(:client) { USI::Client.new("spec/bin/dummy_engine") }
+
+    it "call USI::Engine#command with `usinewgame`" do
+      expect(client).to receive(:command).with("usinewgame")
+      client.usinewgame
+    end
+  end
+
+  describe '#stop' do
+    let(:client) { USI::Client.new("spec/bin/dummy_engine") }
+
+    it "call USI::Engine#command with `stop`" do
+      expect(client).to receive(:command).with("stop")
+      client.stop
+    end
+  end
+
+  describe '#ponderhit' do
+    let(:client) { USI::Client.new("spec/bin/dummy_engine") }
+
+    it "call USI::Engine#command with `ponderhit`" do
+      expect(client).to receive(:command).with("ponderhit")
+      client.ponderhit
+    end
+  end
+
+  describe '#quit' do
+    let(:client) { USI::Client.new("spec/bin/dummy_engine") }
+
+
+    it "call USI::Engine#command with `quit`" do
+      expect(client).to receive(:command).with("quit")
+      client.quit
+    end
+  end
+
   describe '#command' do
     let(:client) { USI::Client.new("spec/bin/dummy_engine") }
 
